62b4da5fc585421cc667e53374b0702100645de3
NEW_NAME <NEW_EMAIL>
2025-04-24 14:22:57 +0100

    patch

--- endpoints/scrape.py
+++ endpoints/scrape.py
@@ -41,7 +41,10 @@ def scrape_article(url):
     rules = SUPPORTED_DOMAINS[domain]
 
     try:
-        res = requests.get(url)
+        headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
+        }
+        res = requests.get(url,headers=headers)
         res.raise_for_status()
         soup = BeautifulSoup(res.text, "html.parser")
 
